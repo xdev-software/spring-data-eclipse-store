4f8693ab214ee50c9b459886e69700cd831b4254
JohannesRabauer <j.rabauer@xdev-software.de>
2024-02-01 07:30:41 +0100

    Removed unused import

--- spring-data-eclipse-store/src/main/java/software/xdev/spring/data/eclipse/store/repository/support/copier/working/RecursiveWorkingCopier.java
+++ spring-data-eclipse-store/src/main/java/software/xdev/spring/data/eclipse/store/repository/support/copier/working/RecursiveWorkingCopier.java
@@ -23,7 +23,6 @@ import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 
 import software.xdev.spring.data.eclipse.store.exceptions.MergeFailedException;
 import software.xdev.spring.data.eclipse.store.repository.IdSetterProvider;
