65ebad74ad299f91f6cfb57508e4fdf4c3bf9923
JohannesRabauer <j.rabauer@xdev-software.de>
2024-10-01 09:44:16 +0200

    Reproducing the issue

--- spring-data-eclipse-store-demo/src/main/java/software/xdev/spring/data/eclipse/store/demo/complex/ComplexConfiguration.java
+++ spring-data-eclipse-store-demo/src/main/java/software/xdev/spring/data/eclipse/store/demo/complex/ComplexConfiguration.java
@@ -1,17 +1,25 @@
 package software.xdev.spring.data.eclipse.store.demo.complex;
 
 import java.nio.file.Path;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.eclipse.store.integrations.spring.boot.types.configuration.EclipseStoreProperties;
 import org.eclipse.store.integrations.spring.boot.types.factories.EmbeddedStorageFoundationFactory;
 import org.eclipse.store.storage.embedded.types.EmbeddedStorage;
 import org.eclipse.store.storage.embedded.types.EmbeddedStorageFoundation;
+import org.eclipse.store.storage.embedded.types.EmbeddedStorageManager;
+import org.eclipse.store.storage.restadapter.types.StorageRestAdapter;
+import org.eclipse.store.storage.restservice.spring.boot.types.configuration.StoreDataRestServiceProperties;
+import org.eclipse.store.storage.restservice.spring.boot.types.rest.StoreDataRestController;
 import org.eclipse.store.storage.types.Storage;
+import org.eclipse.store.storage.types.StorageManager;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.transaction.TransactionManagerCustomizers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import software.xdev.spring.data.eclipse.store.repository.config.EclipseStoreClientConfiguration;
@@ -46,6 +54,24 @@ public class ComplexConfiguration extends EclipseStoreClientConfiguration
 	public EmbeddedStorageFoundation<?> createEmbeddedStorageFoundation()
 	{
 		return EmbeddedStorage.Foundation(Storage.Configuration(Storage.FileProvider(Path.of(STORAGE_PATH))));
+	}
+	
+	@Bean
+	@DependsOn({"embeddedStorageFoundationFactory"})
+	public Map<String, StorageRestAdapter> storageRestAdapters(final Map<String, EmbeddedStorageManager> storages)
+	{
+		return Map.of(
+			"default", StorageRestAdapter.New(this.createEmbeddedStorageFoundation())
+		);
+	}
+	
+	@Bean
+	@DependsOn({"embeddedStorageFoundationFactory"})
+	public StoreDataRestController storageDataRestController(
+		final Map<String, StorageRestAdapter> storageRestAdapters,
+		final StoreDataRestServiceProperties properties)
+	{
+		return new StoreDataRestController(storageRestAdapters, properties);
 	}
 	
 	/**
